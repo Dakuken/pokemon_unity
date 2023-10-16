34fbdbca401b7f26316aec119291001ac7db5bf9
engelma19u <quentin.engelmann8@etu.univ-lorraine.fr>
2023-10-16 12:00:57 +0200

    importation

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,62 +1,78 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+
     public float moveSpeed;
-    
+    public LayerMask solidObjectLayer;
+    public LayerMask grassLayer;
+
     private bool isMoving;
-    private Vector2 input;
+    private Vector2 input; 
 
     private Animator animator;
 
-    private void Awake()
-    {
+    private void Awake(){
         animator = GetComponent<Animator>();
     }
 
 
+    // Update is called once per frame
     private void Update()
     {
-        if (!isMoving)
-        {
+        if(!isMoving){
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
 
-            //remove diagonal
-            if (input.x != 0) input.y = 0;
-            
-            
-            if (input != Vector2.zero)
-            {
+            if (input.x !=0) input.y = 0;
+
+            if(input != Vector2.zero){
                 animator.SetFloat("moveX", input.x);
                 animator.SetFloat("moveY", input.y);
-                
                 var targetPos = transform.position;
                 targetPos.x += input.x;
                 targetPos.y += input.y;
 
+                if(isWalkable(targetPos)){
                 StartCoroutine(Move(targetPos));
+                }
             }
+            
         }
-        
+
         animator.SetBool("isMoving", isMoving);
+        
     }
 
-    IEnumerator Move(Vector3 targetPos)
-    {
+    IEnumerator Move(Vector3 targetPos){
         isMoving = true;
-        
-        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
-        {
+
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon){
             transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
             yield return null;
         }
-
         transform.position = targetPos;
+
         isMoving = false;
+
+        checkForEncounters();
     }
-    
+
+    private bool isWalkable(Vector3 targetPos){
+        if (Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectLayer) != null){
+            return false;
+        } 
+        return true;
+    }
+
+    private void checkForEncounters(){
+        if(Physics2D.OverlapCircle(transform.position, 0.2f, grassLayer) != null){
+            if(Random.Range(1,101) <= 10){
+                Debug.Log("Encountered a wild pokemon");
+            }
+        }
+    }
 }
